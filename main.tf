901c9d9e9ecd53a0989a67390dd68da6293e7ab5
tangent-devop <56938605+tangent-devop@users.noreply.github.com>
2022-02-12 03:49:37 +0000

    Tf file & yml workflows initiated

---
+++ main.tf
@@ -0,0 +1,35 @@
+provider "azurerm" {
+    version = "=2.0.0"
+  features {}
+}
+terraform {
+  backend "azurerm" {
+    resourceresource_group_name = "brd-github-tf-rg"  
+    storage_account_name        = "bradygithub"
+    container_name              = "terraformstatefile"
+    key                         = "terraform.tfstate"  
+    
+  }
+}
+
+resource "azurerm_resource_group" "dev" {
+  name     = "rg-github-tf"
+  location = "UK South"
+}
+
+resource "azurerm_app_service_plan" "dev" {
+  name                =  "bradyappsrvplan"
+  location            =   azurerm_resource_group.dev.location
+  resource_group_name =   azurerm_resource_group.dev.name
+
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
+resource "azurerm_app_service" "dev" {
+  name                =  "bradytfwebapp"
+  location            =   azurerm_resource_group.dev.location
+  resource_group_name =   azurerm_resource_group.dev.name
+  app_service_plan_id =   azurerm_app_service_plan.dev.id
+}
