11e0b89e7dea46c84f784f84260188f347145a5b
tangent-devop <56938605+tangent-devop@users.noreply.github.com>
2022-02-14 04:10:51 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "azurerm" {
     version = "~>2.0.0"
-  features {}
+    features {}
 }
 terraform {
   backend "azurerm" {
-    resourceresource_group_name = "brd-github-tf-rg"  
-    storage_account_name        = "bradygithubstracc"
-    container_name              = "terraformstatefile"
-    key                         = "terraform.tfstate"  
+    resource_group_name  = "brd-github-tf-rg"  
+    storage_account_name = "bradygithubstracc"
+    container_name       = "terraformstatefile"
+    key                  = "terraform.tfstate"  
     
   }
 }
